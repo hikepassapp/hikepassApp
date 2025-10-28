dc23fd42aecfc2b7d9e282adc8792417c28c43a5
Dewanta Rahma Satria <dewantarahmasatria@gmail.com>
2025-10-28 23:56:11 +0700

    Add fitu Informasi

---
+++ lib/app/modules/informasi/widgets/informasi_tab_bar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class InformasiTabBar extends StatelessWidget {
+  final TabController tabController;
+
+  const InformasiTabBar({
+    Key? key,
+    required this.tabController,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: TabBar(
+        controller: tabController,
+        indicator: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.05),
+              blurRadius: 4,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        labelColor: Colors.black,
+        unselectedLabelColor: Colors.grey[600],
+        labelStyle: const TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w600,
+        ),
+        unselectedLabelStyle: const TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+        ),
+        indicatorSize: TabBarIndicatorSize.tab,
+        dividerColor: Colors.transparent,
+        tabs: const [
+          Tab(text: 'Peraturan'),
+          Tab(text: 'Tips'),
+          Tab(text: 'Umum'),
+        ],
+      ),
+    );
+  }
+}
