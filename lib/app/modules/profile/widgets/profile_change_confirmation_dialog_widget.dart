06a6482be9f05398a81733c70414774eb2072456
tjhhh <muhammadfauzan292004@gmail.com>
2025-12-22 22:33:43 +0700

    fixxing the bug at reserrvation

--- lib/app/modules/profile/widgets/profile_change_confirmation_dialog_widget.dart
+++ lib/app/modules/profile/widgets/profile_change_confirmation_dialog_widget.dart
@@ -19,7 +19,6 @@ class ProfileChangeConfirmationDialogWidget extends StatelessWidget {
       content: Text(message),
       actions: [
         TextButton(
-<<<<<<< HEAD
           onPressed: () {
             Navigator.of(context).pop(); // Use this instead of Get.back()
           },
@@ -28,20 +27,6 @@ class ProfileChangeConfirmationDialogWidget extends StatelessWidget {
         ElevatedButton(
           onPressed: () {
             Navigator.of(context).pop(); // Use this instead of Get.back()
-=======
-          onPressed: () => Navigator.of(context).pop(),
-          child: Text(
-            'Tidak',
-            style: TextStyle(
-              color: Colors.grey[600],
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).pop();
->>>>>>> main
             onConfirm();
           },
           child: const Text('Konfirmasi'),
